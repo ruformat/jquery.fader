d477dd8c4cffce00de41995db01a323f10151c60
Shilov Vlad <omgovich@ya.ru>
2013-08-10 05:12:53 -0700

    Оптимизация кода

--- jquery.fader.js
+++ jquery.fader.js
@@ -1,5 +1,9 @@
 (function($){
+
+	// cache window as variable
 	var $window = $(window);
+
+	// plugin function
 	$.fn.fader = function(options){
 	
 		// settings
@@ -11,36 +15,39 @@
 			elements: null
 		}, options);
 
-		// install filterSlider for each items
+		// install for each items
 		this.each(function(){
 	
 			// root block
 			var $block = $(this),
-				isReady = false;
-			
-			if (settings.elements == null){
-				var $sectionShow = $block;
-			} else {
-				var $sectionShow = $block.find(settings.elements);
-			}
+			    isReady = false;
 
-			if($sectionShow.data('show') !=0){
-				$sectionShow.css({'opacity': 0, 'top': settings.offset}).data('show', 0);
-			};
+			// elements
+			var $sectionShow = (settings.elements==null) ? $block : $block.find(settings.elements);
+
+			// hide elements
+			$sectionShow.css({ opacity: 0, top: settings.offset });
 			
+			// {func} show elements
+			var show = function(){
+				isReady = true;
+				$sectionShow.each(function(index){
+					$(this).delay(index*settings.delay).animate({ opacity:1, top: 0 }, settings.duration);
+				});
+			};
+
+			// {func} process scroll 
 			var scroll = function(){
-				var top = $window.scrollTop();
-				if (!isReady && (top - $block.offset().top) > -settings.showPlace){
-					isReady = true;
-					$sectionShow.each(function(index){
-						$(this).delay(index*settings.delay).animate({opacity:'1', top: '0'}, settings.duration);
-					});
-				}
+				if (!isReady) {
+					if ($window.scrollTop()-$block.offset().top > -settings.showPlace) show();
+				}	
 			};
+
+			// init
 			scroll();
+
+			// {event} window scrolling
 			$window.scroll(scroll);
-			
-			
 
 		});
 
